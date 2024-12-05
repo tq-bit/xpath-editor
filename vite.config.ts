4109231972fd78e1fa61d7aceadfe943ab572549
Tq-bit <t.quante@outlook.com>
2024-12-05 10:05:58 +0100

    add gh-pages flow

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true
-  }
+  base: '/xpath-editor/',
 })
