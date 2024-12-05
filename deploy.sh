7d3979a23e5bb3bb4d0420f1044efa959969aab2
Tq-bit <t.quante@outlook.com>
2024-12-05 09:56:53 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,10 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
-
-git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:tq-bit/xmlpath-editor.git main:gh-pages
+git push
 
 cd -
